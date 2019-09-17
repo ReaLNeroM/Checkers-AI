489cc6bba38aff269db76e12f5059412aaa42208
Rowland <rowlandz@outlook.com>
2019-09-16 21:24:14 -0400

    create AI functions

--- AI.java
+++ AI.java
@@ -1,3 +1,11 @@
 public interface AI {
 	Action getBestAction(GameLogic g, State s);
+	
+	Action randomPlay(Model model);
+	
+	Action miniMax(Model model);
+	
+	Action miniMax_a_b(Model model);
+
+	Action h_miniMax_a_b(Model model);
 }
