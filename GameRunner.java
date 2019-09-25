aa0b579c99aee0a2f67edb74a9379101cdb7bbe5
vlade <vlade.maksimovski@gmail.com>
2019-09-24 22:11:46 -0400

    Remove useless files

--- GameRunner.java
+++
@@ -1,7 +0,0 @@
-public class GameRunner {
-    public static void main(String args[]) {
-        CheckersRunner game = new CheckersRunner();
-        game.setupNewGame();
-        game.playGame();
-    }
-}
